745ecb8a2b2a4b9c374e8cf0df594e36a0f0463d
srdcpvtltd <debendraster@gmail.com>
2025-05-19 11:12:15 +0530

    Update index.blade.php

--- resources/views/student/assessment/index.blade.php
+++ resources/views/student/assessment/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-4xl mx-auto py-10">
-        <h1 class="text-3xl font-bold text-center mb-6">?? Assessment</h1>
+        <h1 class="text-3xl font-bold text-center mb-6">Assessment</h1>
 
         @if ($domain)
             {{-- Show Domain Name --}}
