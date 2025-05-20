b6d0d4e63bd82ae5f90db49286cbf4e7350dfcf2
srdcpvtltd <debendraster@gmail.com>
2025-05-20 10:54:36 +0530

    Update create.blade.php

--- resources/views/admin/institute/create.blade.php
+++ resources/views/admin/institute/create.blade.php
@@ -3,7 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800">Create Institute</h2>
     </x-slot>
 
-    <div class="max-w-2xl mx-auto p-4">
+    <div class="max-w-2xl mx-auto p-4" style="width:50%">
         <form method="POST" action="{{ route('institute.store') }}">
             @csrf
 
@@ -19,22 +19,23 @@
                 <x-input-error :messages="$errors->get('address')" class="mt-2" />
             </div>
 
+
             <div>
+                <x-input-label for="contactperson" :value="__('Contact Person')" />
+                <x-text-input id="contactperson" name="contact_person" class="mt-1 block w-full"  required />
+            </div>
+                        <div>
                 <x-input-label for="mobile" :value="__('Mobile')" />
                 <x-text-input id="mobile" type="number"  name="mobile" class="mt-1 block w-full" maxlength="12" required />
                 <x-input-error :messages="$errors->get('mobile')" class="mt-2" />
             </div>
-
               <div>
                 <x-input-label for="allowedstudents" :value="__('Allowed Student')" />
                 <x-text-input id="allowedstudents" type="number"  name="allowed_students" class="mt-1 block w-full" maxlength="12" required />
                 <x-input-error :messages="$errors->get('allowed_students')" class="mt-2" />
             </div>
 
-            <div class="mt-4">
-                <x-input-label for="contactperson" :value="__('Contact Person')" />
-                <textarea id="contactperson" name="contact_person" class="block w-full mt-1 rounded"></textarea>
-            </div>
+
 
             <div class="mt-4">
                 <x-primary-button>{{ __('Create') }}</x-primary-button>
