13608f61e57450274c11e509e1905aeee979eb92
Sk Kahinoor <skkahinoor23@gmail.com>
2025-07-02 12:13:02 +0530

    push before update

--- resources/views/admin/question/index.blade.php
+++ resources/views/admin/question/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex justify-between items-center">
-            <h2 class="text-xl font-semibold text-gray-800">Questions</h2>
+            <h2 class="text-xl font-semibold text-gray-800">Question</h2>
             <a href="{{ route('question.create') }}" class="bg-indigo-600 text-white px-4 py-2 rounded inline-block hover:bg-indigo-700 transition-colors">
                 + Add Question
             </a>
