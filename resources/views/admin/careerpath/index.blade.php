5ec37195c3db47df22cf687e52cb8acb0959655b
skkahinoor <skkahinoor23@gmail.com>
2025-10-14 15:27:41 +0530

    fix menu show issue

--- resources/views/admin/careerpath/index.blade.php
+++ resources/views/admin/careerpath/index.blade.php
@@ -199,7 +199,7 @@
 </div>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         // Initialize tooltips
--- resources/views/student/assessment/result.blade.php
+++ resources/views/student/assessment/result.blade.php
@@ -292,7 +292,7 @@
 
     <!-- Chart.js & Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> --}}
 
 
     {{-- <script>
